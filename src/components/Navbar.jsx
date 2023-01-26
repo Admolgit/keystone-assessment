67f3a676d848a14b74d8e2eb68e0e95cf43eb616
Admolgit <zealous890@gmail.com>
2023-01-26 14:15:05 +0100

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='fixed top-0 pb-[1rem] z-50 left-0 right-0 bg-[#fff] text-white'>Navbar</div>
+    <div className='fixed top-0 pb-[1rem] z-50 left-0 right-0 bg-[#fff] text-white sm-sc:hidden'>Navbar</div>
   )
 }
 
